9bb34620d1abd637108b5272428b21530dcc4323
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-09-13 22:52:04 +0400

    initial content

---
+++ bso-simple-mpa/src/api/product/routes/product.ts
@@ -0,0 +1,8 @@
+/**
+ * product router
+ * Hello this is a test
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::product.product');
---
+++ bso-simple-mpa/src/api/product/services/product.ts
@@ -0,0 +1,8 @@
+/**
+ * product service
+ * Hello again!
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::product.product');
